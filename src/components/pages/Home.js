2dffbe7afe4e702bc49131ee1bda1bd59d706a75
Robby Dongre <86415240+robby-devo@users.noreply.github.com>
2022-04-03 12:21:56 +0530

    uploading user panel

---
+++ src/components/pages/Home.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    console.log("Kaisa hai bro");
+    loadUsers();
+  }, []);
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:3003/users");
+
+    setUser(result.data);
+  };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:3003/users/${id}`);
+    loadUsers();
+  };
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table class="table table-dark table-hover">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Role</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={`/users/edit/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
